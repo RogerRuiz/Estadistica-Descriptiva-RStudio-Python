de2c896f1941dd56d793b36c218f00a59264cbfa
Roger Ruiz <rogerruiziandres@gmail.com>
2020-04-30 15:42:08 +0200

    seguiment curs

--- Script-proves.R
+++ Script-proves.R
@@ -1,1 +1,23 @@
 #Proves
+
+#sapply
+
+x <- 1:10
+sqrt(x)
+
+suma.1 <- function(x){
+  x + 1
+}
+sapply(x, FUN = suma.1)
+
+#Ejemplo que sapply es útil
+
+R.value = function(x){summary(lm((1:4)~c(1:3,x)))$r.squared}
+R.value(x)
+sapply(x, FUN = R.value)
+
+cuadrado = function(x){x^2}
+
+v <- 1:10
+
+cuadrado(v)
